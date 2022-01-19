cfe6bb1b7129c1fc23aef34aa677c2319afd3c1e
Italo-Neves <italorei7@gmail.com>
2022-01-19 16:43:20 -0300

    implementando modelos de formulario

--- passagens/views.py
+++ passagens/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from passagens.forms import PassagemForms
+from passagens.forms import PassagemForms, PessoaForms
 # Create your views here.
 def index(request):
     form = PassagemForms()
@@ -9,10 +9,12 @@ def index(request):
 def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
+        pessoa_forms = PessoaForms(request.POST)
+        pessoa_forms = PessoaForms() 
         if form.is_valid(): 
-            contexto = {'form':form}
+            contexto = {'form':form,'pessoa_form':pessoa_forms}
             return render(request, 'minha_consulta.html', contexto)
         else:
             print('Fomulário inválido')
-            contexto = {'form':form}
+            contexto = {'form':form,'pessoa_form':pessoa_forms}
             return render(request, 'index.html', contexto)
